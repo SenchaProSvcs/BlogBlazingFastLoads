592c065f9a123976f04ecc62fb50b9baacac9c64
Arthur Kay <arthurakay@gmail.com>
2014-08-04 14:47:24 -0500

    Updated compile script, renamed "all" file.

--- build.sh
+++ build.sh
@@ -1,37 +1,20 @@
 # See full list of options: http://docs.sencha.com/extjs/4.2.1/#!/guide/command_app_multi
 
-sencha compile -classpath=touch/src \
-    union -r --file=app.js and  \
-    save allcode and \
-\
-    exclude -all and \
-    include -tag Page2  and \
-    include --namespace Ext.dataview and \
-    concat -cl build/chunked/page2.js and \
-\
-    restore allcode and \
-    exclude -tag Page2  and \
-    exclude --namespace Ext.dataview and \
-    concat -cl build/chunked/all-common-classes.js
+sencha compile \
+        union --recursive --file=app.js and  \
+        save allcode and \
+    \
+        exclude --all and \
+        include --tag Page2  and \
+        include --namespace Ext.dataview and \
+        save page2 and \
+    \
+        concat -y build/chunked/page2.js and \
+    \
+        restore allcode and \
+        exclude --set page2  and \
+    \
+        concat -y build/chunked/rest-of-app-and-touch.js
 
 # Insert "Build indicator" into code
-echo "ChunkCompile.isBuilt = true;" >> build/chunked/all-common-classes.js
-
-
-# some other options
-
-# restore asp and \
-# exclude -set boot,common and \
-# concat -out build/ttt.js and \
-# \
-# restore zzz and \
-# exclude -set boot,common and \
-# concat -out build/zzz.js and \
-# \
-# restore sdfg and \
-# exclude -set boot,common and \
-# concat -out build/sdfg.js and \
-# \
-# restore sdf and \
-# exclude -set boot,common and \
-# concat -out build/sdf.js and \
+echo "ChunkCompile.isBuilt = true;" >> build/chunked/rest-of-app-and-touch.js
